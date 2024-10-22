ad41de4ccbadd9ee1e35d62a3432643e51c619c0
Varun0157 <varun.edachali@gmail.com>
2024-10-22 21:52:27 +0530

    data: script to move data to local store

--- scripts/download-data.py
+++ scripts/download-data.py
@@ -3,4 +3,27 @@ import kagglehub
 # Download latest version
 path = kagglehub.dataset_download("gowrishankarp/newspaper-text-summarization-cnn-dailymail")
 
-print("Path to dataset files:", path)
+print("path to dataset files:", path)
+
+# move the downloaded files and directories from path to the local save path
+local_data_path = "./data"
+
+print("copying files and directories from", path, "to", local_data_path)
+
+import shutil
+import os
+
+def move_files_and_directories(src, dst):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    for item in os.listdir(src):
+        source = os.path.join(src, item)
+        destin = os.path.join(dst, item)
+        
+        if os.path.isdir(source):
+            shutil.copytree(source, destin, dirs_exist_ok=True)
+            shutil.rmtree(source)
+        else:
+            shutil.move(source, destin)
+
+move_files_and_directories(path, local_data_path)
