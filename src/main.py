5fd521bd17bba50dbdccfc288b1e4ef310d8cd39
Varun0157 <varun.edachali@gmail.com>
2024-10-24 23:33:30 +0530

    feat: attempt at test and rouge for best model (untested)

--- src/main.py
+++ src/main.py
@@ -4,9 +4,13 @@ from transformers import GPT2Tokenizer
 
 from src.data import create_dataloader
 from src.traditional import GPTNeoForSummarization
-from src.utils import train_and_validate
+from src.utils import train_and_validate, test
 
 MAX_BATCH_IN_MEM = 1
+DESIRED_BATCH_SIZE = 64
+
+assert DESIRED_BATCH_SIZE % MAX_BATCH_IN_MEM == 0
+ACCUMULATION_STEPS = DESIRED_BATCH_SIZE / MAX_BATCH_IN_MEM
 
 
 def main():
@@ -28,9 +32,19 @@ def main():
 
     train_and_validate(model, train_loader, valid_dataloader, num_epochs=10, lr=5e-3)
 
-    max_gpu_mb_alloc = torch.cuda.max_memory_allocated() / 1024 ** 2
+    max_gpu_mb_alloc = torch.cuda.max_memory_allocated() / 1024**2
     print(f"Max GPU memory allocated: {max_gpu_mb_alloc:.2f} MB")
-    # torch.cuda.reset_peak_memory_stats() also exists, may be useful 
+    # torch.cuda.reset_peak_memory_stats() also exists, may be useful
+
+    # todo: separate the train and test later
+    test_dataloader = create_dataloader(
+        "./data/cnn_dailymail/test.csv", tokenizer, batch_size=MAX_BATCH_IN_MEM
+    )
+    checkpoint = torch.load("best_model.pth", weights_only=True)
+    model.load_state_dict(checkpoint)
+
+    test(model, tokenizer, test_dataloader)
+
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,15 @@ import torch
 import torch.nn as nn
 from torch.optim import AdamW
 
+from evaluate import load
 
-def _train(model, dataloader, criterion, optimizer):
+
+def _train(model, dataloader, criterion, optimizer, accumulation_steps: int = 1):
     model.train()
+    optimizer.zero_grad()
 
     total_loss = 0
-    for batch in dataloader:
-        optimizer.zero_grad()
+    for step, batch in enumerate(dataloader):
 
         input_ids = batch["input_ids"].to(model.device)
         attention_mask = batch["attention_mask"].to(model.device)
@@ -23,10 +25,12 @@ def _train(model, dataloader, criterion, optimizer):
         labels = labels.view(-1)
 
         loss = criterion(logits, labels)
+        loss.backward()
         total_loss += loss.item()
 
-        loss.backward()
-        optimizer.step()
+        if (step + 1) % accumulation_steps == 0 or (step + 1) == len(dataloader):
+            optimizer.step()
+            optimizer.zero_grad()
 
     return total_loss
 
@@ -53,7 +57,12 @@ def _validate(model, dataloader, criterion):
 
 
 def train_and_validate(
-    model, train_dataloader, valid_dataloader, num_epochs=3, lr=5e-4
+    model,
+    train_dataloader,
+    valid_dataloader,
+    num_epochs=3,
+    lr=5e-4,
+    accumulation_steps: int = 1,
 ):
     model.train()
 
@@ -66,7 +75,13 @@ def train_and_validate(
     best_valid_loss = float("inf")
     for epoch in range(num_epochs):
         start_time = time.time()
-        train_loss = _train(model, train_dataloader, criterion, optimizer)
+        train_loss = _train(
+            model=model,
+            dataloader=train_dataloader,
+            criterion=criterion,
+            optimizer=optimizer,
+            accumulation_steps=accumulation_steps,
+        )
         valid_loss = _validate(model, valid_dataloader, criterion)
         end_time = time.time()
 
@@ -82,5 +97,40 @@ def train_and_validate(
         print("\tbest model saved")
 
 
-def _test(model, test_dataloader):
-    pass
+def test(model, tokenizer, test_dataloader):
+    model.eval()
+
+    criterion = nn.CrossEntropyLoss(ignore_index=-100)
+
+    all_predictions, all_references = [], []
+
+    total_loss = 0
+    for batch in test_dataloader:
+        input_ids = batch["input_ids"].to(model.device)
+        attention_mask = batch["attention_mask"].to(model.device)
+        labels = batch["labels"].to(model.device)
+
+        with torch.no_grad():
+            logits = model(input_ids, attention_mask)
+            loss = criterion(logits.view(-1, logits.shape[-1]), labels.view(-1))
+            total_loss += loss.item()
+
+            predictions = logits.argmax(dim=-1).squeeze(0).tolist()
+            references = labels.squeeze(0).tolist()
+
+            all_predictions.extend(predictions)
+            all_references.extend(references)
+
+    print(f"test loss: {total_loss}")
+
+    rouge = load("rouge")
+    results = rouge.compute(
+        predictions=[tokenizer.decode(all_predictions)],
+        references=tokenizer.decode(all_references),
+    )
+    if results is None:
+        print("unable to calculate rouge score")
+    else:
+        print("rouge scores ->")
+        for key, val in results:
+            print(f"\t{key}: {val}")
