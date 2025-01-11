d8e9109b1cac8486629a83ae650832a1b423021a
Varun0157 <varun.edachali@gmail.com>
2025-01-11 09:30:43 +0530

    refactor: remove un-necessary files, move inference sample to src

--- scripts/tokenizer.py
+++
@@ -1,5 +0,0 @@
-from transformers import GPT2Tokenizer
-
-# Download and save the tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125m")
-tokenizer.save_pretrained("./model/tokenizer")
